e093961aabffe94900ea5ac7a18e8220ee09879e
OtabekATW <otabektursunov468@gmail.com>
2023-12-22 16:01:25 +0500

    05.filter_products_by_category.py

---
+++ 05.filter_products_by_category.py
@@ -0,0 +1,11 @@
+def filter_products_by_category(product_data, category):
+    """
+    Filter products by category and return a new list of lists containing only the products from the specified category.
+
+    Args:
+        product_data (list): A list of lists representing product data.
+        category (str): The category to filter by.
+
+    Returns:
+        list: A filtered list of lists containing products from the specified category.
+    """
