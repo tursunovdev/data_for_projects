14ea74e073a9bef970a4a1a3fc7796bd0103f254
OtabekATW <otabektursunov468@gmail.com>
2023-12-22 16:00:46 +0500

    03.find_most_expensive_product.py

---
+++ 03.find_most_expensive_product.py
@@ -0,0 +1,10 @@
+def find_most_expensive_product(product_data):
+    """
+    Find the most expensive product.
+
+    Args:
+        product_data (list): A list of lists representing product data.
+
+    Returns:
+        list: A list representing the most expensive product (name and price).
+    """
