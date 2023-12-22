f49bcd5c6448c443e41b11c8de28d4b44b2d9f4b
OtabekATW <otabektursunov468@gmail.com>
2023-12-22 16:00:22 +0500

    02.calculate_total_number_of_products.py

---
+++ 02.calculate_total_number_of_products.py
@@ -0,0 +1,10 @@
+def calculate_total_number_of_products(product_data):
+    """
+    Calculate the total number of products.
+
+    Args:
+        product_data (list): A list of lists representing product data.
+
+    Returns:
+        int: The total number of products.
+    """
