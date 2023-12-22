ffb464167fbbbec5c7fd313e8db27ee976b2a2de
OtabekATW <otabektursunov468@gmail.com>
2023-12-22 16:01:45 +0500

    06.search_product_by_keyword.py

---
+++ 06.search_product_by_keyword.py
@@ -0,0 +1,11 @@
+def search_product_by_keyword(product_data, keyword):
+    """
+    Search for a product by keyword and return a new list of lists containing matching products.
+
+    Args:
+        product_data (list): A list of lists representing product data.
+        keyword (str): The keyword to search for in product names.
+
+    Returns:
+        list: A filtered list of lists containing products with names matching the keyword.
+    """
