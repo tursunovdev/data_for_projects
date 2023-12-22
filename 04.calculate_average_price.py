d6800bd9511da8bad356ed9958615e3181e9e67a
OtabekATW <otabektursunov468@gmail.com>
2023-12-22 16:01:03 +0500

    04.calculate_average_price.py

---
+++ 04.calculate_average_price.py
@@ -0,0 +1,10 @@
+def calculate_average_price(product_data):
+    """
+    Calculate the average price of all products.
+
+    Args:
+        product_data (list): A list of lists representing product data.
+
+    Returns:
+        float: The average price of all products.
+    """
